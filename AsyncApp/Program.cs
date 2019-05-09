4ff34af856b6a56e5d89057198175ebc97cf15e8
唐林 <47147551@qq.com>
2019-05-09 17:58:45 +0800

    测试一下

---
+++ AsyncApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)        {
+          
+
+            Task task = DemoCompletedAsync();
+            Console.WriteLine("Method returned");
+            task.Wait();
+            Console.WriteLine("Task completed");
+        }
+
+        static async Task DemoCompletedAsync()
+        {
+            Console.WriteLine("Before first await");
+            await Task.FromResult(10);
+            Console.WriteLine("Between awaits");
+            await Task.Delay(1000);
+            Console.WriteLine("After second await");
+        }
+    }
+}
---
+++ ExpressionApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Expression<Func<int, int, int>> adder = (x, y) => x + y;
+            Console.WriteLine(adder);
+
+            // handwriten
+            ParameterExpression xParameter = Expression.Parameter(typeof(int), "x");
+            ParameterExpression yParameter = Expression.Parameter(typeof(int), "y");
+            Expression body = Expression.Add(xParameter, yParameter);
+            ParameterExpression[] parameters = new [] { xParameter, yParameter };
+            Expression<Func<int, int, int>> adder2 = Expression.Lambda<Func<int, int, int>>(body, parameters);
+
+            Console.WriteLine(adder2);
+
+            //如果lambad具有语句表达体，则无法转化为Express
+            //Expression<Func<int, int, int>> adder3 = (x, y) => { return x + y; };
+            
+           
+
+            Console.ReadLine();
+        }
+    }
+}
